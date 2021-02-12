a5cd20ce061ca690c9a69fd9c8d859020ce95583
MateKovacs <netmate12@gmail.com>
2021-02-12 12:14:47 +0100

    Második feladat

--- src/fuvar/Fuvar.java
+++ src/fuvar/Fuvar.java
@@ -1,11 +1,26 @@
+
 package fuvar;
 
 
 public class Fuvar {
+    private int taxi_id;
+    private String indulas;
+    private int idotartam;
+    private double tavolsag;
+    private double viteldij;
+    private double borravalo;
+    private String fizetes_modja;
 
-    
-    public static void main(String[] args) {
-        
+    public Fuvar(String sor) {
+        String[] s = sor.split(";");
+        this.taxi_id = Integer.parseInt(s[0]);
+        this.indulas = s[1];
+        this.idotartam = Integer.parseInt(s[2]);
+        this.tavolsag = Double.parseDouble(s[3]);
+        this.viteldij = Double.parseDouble(s[4]);
+        this.borravalo = Double.parseDouble(s[5]);
+        this.fizetes_modja = s[6];
     }
+    
     
 }
